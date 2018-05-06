774457da0382c0f11b2647c04333186ff5dde3d7
Juri Kostik <juri.kostik@web-se.de>
2018-05-06 22:15:55 +0200

    [FEATURE] server with request handle and routing

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 var http = require("http");
 var url = require("url");
 
-
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-      var pathName = url.parse(request.url).pathname;
-      console.log("Path name: " + pathName);
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
+    route(handle, pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
   }
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
 
-function route(pathname) {
-    console.log("About to route a request for " + pathname);
-}
-  
-exports.route = route;
 exports.start = start;
