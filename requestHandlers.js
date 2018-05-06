976986afe3dcc48b8299920beac3daf1edf50328
Juri Kostik <juri.kostik@web-se.de>
2018-05-06 23:02:41 +0200

    [TASK] send response an a handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,13 @@
-function start() {
-    console.log("Request handler 'start' was called.");
+function start(response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Startseite aufgerufen! Herzlich willkommen!");
+    response.end();
   }
   
-  function upload() {
-    console.log("Request handler 'upload' was called.");
+  function upload(response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Upload-Funktion aufgerufen! Bitte wählen Sie ein Foto aus und laden Sie es hoch.");
+    response.end();
   }
   
   exports.start = start;
